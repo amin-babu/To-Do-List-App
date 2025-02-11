5b570fa764975c410d64eba700d11df20bea4798
aminbabu12 <badrulaminbabu@gmail.com>
2025-02-11 18:50:32 +0600

    delet btn added

--- script.js
+++ script.js
@@ -22,8 +22,19 @@ function addTaskToDom(taskText){
   li.innerHTML = `
   <span>${taskText}</span>
   <span>
-      <span class="delete btn btn-danger fs-6">Delete</span>
+      <span class="delete btn btn-danger fs-6" onclick="deleteTask(this)">Delete</span>
   </span>
   `;
   ul.appendChild(li);
+
+  
+};
+
+function deleteTask(element){
+  let li = element.parentElement.parentElement;
+  let taskText = li.firstElementChild.innerHTML;
+  li.remove();
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks = tasks.filter(task => task !== taskText);
+  localStorage.setItem("tasks",JSON.stringify(tasks));
 };
