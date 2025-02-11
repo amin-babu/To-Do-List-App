7140faa3dc18679b971a6bb7b17f22e827b5687b
aminbabu12 <badrulaminbabu@gmail.com>
2025-02-11 11:51:53 +0600

    DOM Done

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function addTask(){
   let textInput = document.getElementById('taskInput');
   let taskText = textInput.value;
+
+  let ul = document.getElementById('taskList');
+  // console.log(ul);
+  let li = document.createElement('li');
+  li.innerHTML = `
+  <span>${taskText}</span>
+  `;
+  ul.appendChild(li);
 };
