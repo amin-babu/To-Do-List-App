caae8943c59548c1f8ad39a18dd75643f3d8f49b
aminbabu12 <badrulaminbabu@gmail.com>
2025-02-11 18:34:44 +0600

    again

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+document.addEventListener('DOMContentLoaded', loadTask);
+
+function loadTask(){
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.forEach(task => addTaskToDom(task));
+};
+
 function addTask(){
   let textInput = document.getElementById('taskInput');
   let taskText = textInput.value;
+  addTaskToDom(taskText);
 
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.push(taskText);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  textInput.value = '';
+};
+
+function addTaskToDom(taskText){
   let ul = document.getElementById('taskList');
-  // console.log(ul);
   let li = document.createElement('li');
   li.innerHTML = `
   <span>${taskText}</span>
+  <span>
+      <span class="delete btn btn-danger fs-6">Delete</span>
+  </span>
   `;
   ul.appendChild(li);
 };
